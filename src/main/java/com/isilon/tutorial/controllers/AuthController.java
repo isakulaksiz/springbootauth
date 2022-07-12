170e327a8b3b3a3b551d9853432a7954895da664
isa kulaksız <isakulaksiz@outlook.com>
2022-07-12 12:33:00 +0300

    added signup controller and data model

--- src/main/java/com/isilon/tutorial/controllers/AuthController.java
+++ src/main/java/com/isilon/tutorial/controllers/AuthController.java
@@ -1,8 +1,12 @@
 package com.isilon.tutorial.controllers;
 
+import com.isilon.tutorial.model.ERole;
+import com.isilon.tutorial.model.Role;
 import com.isilon.tutorial.model.User;
 import com.isilon.tutorial.payload.request.LoginRequest;
+import com.isilon.tutorial.payload.request.SignupRequest;
 import com.isilon.tutorial.payload.response.JwtResponse;
+import com.isilon.tutorial.payload.response.MessageResponse;
 import com.isilon.tutorial.repository.RoleRepository;
 import com.isilon.tutorial.repository.UserRepository;
 import com.isilon.tutorial.security.jwt.JwtUtils;
@@ -17,7 +21,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
@@ -57,4 +63,58 @@ public class AuthController {
                 userDetails.getEmail(), roles));
     }
 
+    @PostMapping("/signup")
+    public ResponseEntity<?> registerUser(@Valid @RequestBody SignupRequest signUpRequest) {
+        if (userRepository.existsByUsername(signUpRequest.getUsername())) {
+            return ResponseEntity
+                    .badRequest()
+                    .body(new MessageResponse("Error: Username is already taken!"));
+        }
+
+        if (userRepository.existsByEmail(signUpRequest.getEmail())) {
+            return ResponseEntity
+                    .badRequest()
+                    .body(new MessageResponse("Error: Email is already in use!"));
+        }
+
+        // Create new user's account
+        User user = new User(signUpRequest.getUsername(),
+                signUpRequest.getEmail(),
+                passwordEncoder.encode(signUpRequest.getPassword()));
+
+        Set<String> strRoles = signUpRequest.getRole();
+        Set<Role> roles = new HashSet<>();
+
+        if (strRoles == null) {
+            Role userRole = roleRepository.findByName(ERole.ROLE_USER)
+                    .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+            roles.add(userRole);
+        } else {
+            strRoles.forEach(role -> {
+                switch (role) {
+                    case "admin":
+                        Role adminRole = roleRepository.findByName(ERole.ROLE_ADMIN)
+                                .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+                        roles.add(adminRole);
+
+                        break;
+                    case "mod":
+                        Role modRole = roleRepository.findByName(ERole.ROLE_MODERATOR)
+                                .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+                        roles.add(modRole);
+
+                        break;
+                    default:
+                        Role userRole = roleRepository.findByName(ERole.ROLE_USER)
+                                .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+                        roles.add(userRole);
+                }
+            });
+        }
+
+        user.setRoles(roles);
+        userRepository.save(user);
+
+        return ResponseEntity.ok(new MessageResponse("User registered successfully!"));
+    }
 }
